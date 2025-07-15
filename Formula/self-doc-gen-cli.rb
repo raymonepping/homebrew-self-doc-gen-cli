592e07bed40301341408befcd895cde46d3c414c
Raymon Epping <your@email.com>
2025-07-15 20:21:39 +0200

    15/07/2025 - Updated configuration and fixed bugs

--- Formula/self-doc-gen-cli.rb
+++ Formula/self-doc-gen-cli.rb
@@ -1,7 +1,7 @@
 class SelfDocGenCli < Formula
   desc "Self-documenting CLI generator with markdown templates and folder visualization"
   homepage "https://github.com/raymonepping/self_doc_gen_cli"
-  url "https://github.com/raymonepping/self_doc_gen_cli/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/raymonepping/homebrew-self-doc-gen-cli/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   version "1.0.0"
